445f4ff276a58e47c594f0db0c0698f8f31a20a9
Daniel <daniel.roncal.87@gmail.com>
2019-06-07 15:49:34 -0500

    Se agrego update de tipos

--- update.go
+++ update.go
@@ -1,29 +1,30 @@
 package dal
 
 import (
+	"encoding/json"
 	"reflect"
 	"strings"
 )
 
 /**
 UPDATE Section
- */
+*/
 
 type UpdateBuilder struct {
 	b *Builder
 }
 
 func (b *UpdateBuilder) Set(column, parameter string) *UpdateBuilder {
-	p := columnPartSQL{part:columnsPartEnum}
+	p := columnPartSQL{part: columnsPartEnum}
 
 	if p.parts == nil {
 		p.parts = make([]columnSQL, 1)
 	}
-	p.parts[0] = columnSQL{name: column, parameter:parameter}
+	p.parts[0] = columnSQL{name: column, parameter: parameter}
 
 	if ok, part := b.b.getPart(columnsPartEnum); ok {
 		parts := part.(columnPartSQL).parts
-		p.parts = append(parts, columnSQL{name: column, parameter:parameter})
+		p.parts = append(parts, columnSQL{name: column, parameter: parameter})
 
 		b.b.removePart(columnsPartEnum)
 		b.b.sqlParts = append(b.b.sqlParts, p)
@@ -39,11 +40,11 @@ func (b *UpdateBuilder) Sets(columns ...string) *UpdateBuilder {
 		return b
 	}
 
-	p := columnPartSQL{part:columnsPartEnum}
+	p := columnPartSQL{part: columnsPartEnum}
 
 	p.parts = make([]columnSQL, len(columns))
 	for c, i := range columns {
-		p.parts[c] = columnSQL{name:i, parameter:"?"}
+		p.parts[c] = columnSQL{name: i, parameter: "?"}
 	}
 
 	if ok, part := b.b.getPart(columnsPartEnum); ok {
@@ -66,24 +67,43 @@ func (b *UpdateBuilder) Type(entity interface{}) *UpdateBuilder {
 	count := 0
 	var id interface{}
 	for i := 0; i < e.NumField(); i++ {
-		columnsConfig := strings.Split(e.Type().Field(i).Tag.Get("db"), ", ")
+		dbConfig := strings.Replace(e.Type().Field(i).Tag.Get("db"), " ", "", -1)
+		columnsConfig := strings.Split(dbConfig, ",")
 		columnName := columnsConfig[0]
+		value := e.Field(i).Interface()
+
+		config := make(map[string]bool)
+		for _, v := range columnsConfig {
+			config[v] = true
+		}
+
+		if value == nil || config["autoincrement"] || config["omitted"] {
+			continue
+		}
 
 		if columnName == "" {
 			columnName = e.Type().Field(i).Name
 		}
 
-		value := e.Field(i).Interface()
 		if columnName == "id" {
 			value = int64(value.(int64))
 			id = value
 		}
 
-		if len(columnsConfig) > 1 && columnsConfig[1] != "autoincrement" || len(columnsConfig) == 1 {
-			columnNames = append(columnNames, columnName)
-			b.SetParameter(count, value)
-			count += 1
+		if config["json"] || config["jsonb"] {
+			if reflect.ValueOf(value).IsNil() {
+				continue
+			}
+			if byts, err := json.Marshal(value); err != nil {
+				panic("la columna: " + columnName + " no contiene un valor válido")
+			} else {
+				value = string(byts)
+			}
 		}
+
+		columnNames = append(columnNames, columnName)
+		b.SetParameter(count, value)
+		count += 1
 	}
 
 	b.Sets(columnNames...)
@@ -141,18 +161,18 @@ func (b *UpdateBuilder) SetParameter(p, v interface{}) *UpdateBuilder {
 
 /**
 PRIVATE methods
- */
+*/
 
 func (b *UpdateBuilder) addWhere(conditiontype, condition string) *UpdateBuilder {
 	p := wherePartSQL{}
 	if p.parts == nil {
 		p.parts = make([]whereContainer, 1)
 	}
-	p.parts[0] = whereContainer{conditionType:conditiontype, Where: &Where{condition:condition}}
+	p.parts[0] = whereContainer{conditionType: conditiontype, Where: &Where{condition: condition}}
 
 	if ok, part := b.b.getPart(wherePartEnum); ok {
 		parts := part.(wherePartSQL).parts
-		p.parts = append(parts, whereContainer{conditionType:conditiontype, Where: &Where{condition:condition}})
+		p.parts = append(parts, whereContainer{conditionType: conditiontype, Where: &Where{condition: condition}})
 
 		b.b.removePart(wherePartEnum)
 		b.b.sqlParts = append(b.b.sqlParts, p)
